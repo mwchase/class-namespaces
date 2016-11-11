50f64942514d500b28635ddd45f3c7cc844cd846
Max Chase <max.chase@gmail.com>
2016-11-10 22:26:05 -0500

    First attempt at setup.py.

---
+++ setup.py
@@ -0,0 +1,53 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='class_namespaces',
+
+    version='0.1.0',
+
+    description='Class Namespaces',
+    long_description=long_description,
+
+    url='https://github.com/mwchase/class-namespaces',
+
+    author='Max Woerner Chase',
+    author_email='max.chase@gmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='class namespaces',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    extras_require={
+        'test': ['coverage', 'pytest'],
+    },
+)
