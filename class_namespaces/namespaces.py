199334df8db7e48d5d35fa6c9c729ea4a07cae45
Max Chase <max.chase@gmail.com>
2016-11-21 15:43:06 -0500

    Break it up more.

--- class_namespaces/namespaces.py
+++ class_namespaces/namespaces.py
@@ -12,9 +12,11 @@ import functools
 import itertools
 import weakref
 
+from . import ops
 from .descriptor_inspector import _DescriptorInspector
 from .flags import ENABLE_SET_NAME
 from .proxy import _Proxy
+from .scope_proxy import _ScopeProxy
 
 
 _PROXY_INFOS = weakref.WeakKeyDictionary()
@@ -60,41 +62,6 @@ def _retarget(ns_proxy):
     return ns_proxy
 
 
-def _get(a_map, name):
-    """Return a _DescriptorInspector around the attribute, or None."""
-    try:
-        value = a_map[name]
-    except KeyError:
-        return None
-    else:
-        return _DescriptorInspector(value)
-
-
-def _delete(dct, name):
-    """Attempt to delete `name` from `dct`. Raise AttributeError if missing."""
-    try:
-        del dct[name]
-    except KeyError:
-        raise AttributeError(name)
-
-
-def _has_get(value):
-    """Return whether the value is a wrapped getter descriptor."""
-    return value is not None and value.has_get
-
-
-def _is_data(value):
-    """Return whether the value is a wrapped data descriptor."""
-    return value is not None and value.is_data
-
-
-def _get_data(dct, name):
-    """Return the data descriptor associated with `name` in `dct`, if any."""
-    value = _get(dct, name)
-    if _is_data(value):
-        return value
-
-
 class _NamespaceProxy(_Proxy):
 
     """Proxy object for manipulating and querying namespaces."""
@@ -112,15 +79,15 @@ class _NamespaceProxy(_Proxy):
         _, instance, owner = _PROXY_INFOS[self]
         instance_map = _instance_map(self)
         mro_map = _mro_map(self)
-        instance_value = _get(instance_map, name)
-        mro_value = _get(mro_map, name)
-        if _is_data(mro_value):
+        instance_value = ops._get(instance_map, name)
+        mro_value = ops._get(mro_map, name)
+        if ops._is_data(mro_value):
             return mro_value.get(instance, owner)
-        elif issubclass(owner, type) and _has_get(instance_value):
+        elif issubclass(owner, type) and ops._has_get(instance_value):
             return instance_value.get(None, instance)
         elif instance_value is not None:
             return instance_value.object
-        elif _has_get(mro_value):
+        elif ops._has_get(mro_value):
             return mro_value.get(instance, owner)
         elif mro_value is not None:
             return mro_value.object
@@ -135,7 +102,7 @@ class _NamespaceProxy(_Proxy):
             real_map[name] = value
             return
         mro_map = _mro_map(self)
-        target_value = _get_data(mro_map, name)
+        target_value = ops._get_data(mro_map, name)
         if target_value is not None:
             target_value.set(instance, value)
             return
@@ -147,48 +114,14 @@ class _NamespaceProxy(_Proxy):
         dct, instance, owner = _PROXY_INFOS[self]
         real_map = Namespace.get_namespace(owner, dct.path)
         if instance is None:
-            _delete(real_map, name)
+            ops._delete(real_map, name)
             return
-        value = _get_data(real_map, name)
+        value = ops._get_data(real_map, name)
         if value is not None:
             value.delete(instance)
             return
         instance_map = Namespace.get_namespace(instance, dct.path)
-        _delete(instance_map, name)
-
-
-class _ScopeProxy(_Proxy):
-
-    """Proxy object for manipulating namespaces during class creation."""
-
-    __slots__ = '__weakref__',
-
-    def __init__(self, dct, container):
-        _PROXY_INFOS[self] = container
-        container[self] = dct
-
-    def __dir__(self):
-        return _PROXY_INFOS[self][self]
-
-    def __getattribute__(self, name):
-        dct = _PROXY_INFOS[self][self]
-        try:
-            return dct[name]
-        except KeyError:
-            raise AttributeError(name)
-
-    def __setattr__(self, name, value):
-        _PROXY_INFOS[self][self][name] = value
-
-    def __delattr__(self, name):
-        _delete(_PROXY_INFOS[self][self], name)
-
-    def __enter__(self):
-        return _PROXY_INFOS[self][self].__enter__()
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        return _PROXY_INFOS[self][self].__exit__(
-            exc_type, exc_value, traceback)
+        ops._delete(instance_map, name)
 
 
 class Namespace(dict):
---
+++ class_namespaces/scope_proxy.py
@@ -0,0 +1,42 @@
+"""Base class for Namespace proxies in class creation."""
+
+import weakref
+
+from . import ops
+from .proxy import _Proxy
+
+_PROXY_INFOS = weakref.WeakKeyDictionary()
+
+
+class _ScopeProxy(_Proxy):
+
+    """Proxy object for manipulating namespaces during class creation."""
+
+    __slots__ = '__weakref__',
+
+    def __init__(self, dct, container):
+        _PROXY_INFOS[self] = container
+        container[self] = dct
+
+    def __dir__(self):
+        return _PROXY_INFOS[self][self]
+
+    def __getattribute__(self, name):
+        dct = _PROXY_INFOS[self][self]
+        try:
+            return dct[name]
+        except KeyError:
+            raise AttributeError(name)
+
+    def __setattr__(self, name, value):
+        _PROXY_INFOS[self][self][name] = value
+
+    def __delattr__(self, name):
+        ops._delete(_PROXY_INFOS[self][self], name)
+
+    def __enter__(self):
+        return _PROXY_INFOS[self][self].__enter__()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        return _PROXY_INFOS[self][self].__exit__(
+            exc_type, exc_value, traceback)
