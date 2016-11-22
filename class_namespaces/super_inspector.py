a88729c80c1ec31d5ba9a5834be1417f512a60ac
Max Chase <max.chase@gmail.com>
2016-11-22 00:10:32 -0500

    Add methods.

--- class_namespaces/super_inspector.py
+++ class_namespaces/super_inspector.py
@@ -17,3 +17,12 @@ class _SuperInspector(_Inspector):
         thisclass, self_class = super_obj.thisclass, super_obj.self_class
         mro = self_class.__mro__[self_class.__mro__.index(thisclass) + 1:]
         return super().__new__(cls, super_obj, mro)
+
+    def getattribute(self, name):
+        return self.get_as_attribute('__getattribute__')(self.object, name)
+
+    def setattr(self, name, value):
+        self.get_as_attribute('__setattr__')(self.object, name, value)
+
+    def delattr(self, name):
+        self.get_as_attribute('__delattr__')(self.object, name)
