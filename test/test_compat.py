0a3b7239701057ce0be56c754a846b62263e6636
Max Chase <max.chase@gmail.com>
2016-12-02 12:37:45 -0500

    Need the type.

--- test/test_compat.py
+++ test/test_compat.py
@@ -6,3 +6,7 @@ def test_import_compat(compat):
 def test_import_abc(abc):
     """Test the module is importable. See conftest.py for the actual import."""
     assert abc
+
+
+def test_has_class(abc):
+    assert abc.NamespaceableABC
