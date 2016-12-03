8a30a9a124b78c140b69f786a83093f6fb84f075
Max Chase <max.chase@gmail.com>
2016-12-02 23:25:29 -0500

    Blank line before docstring.

--- class_namespaces/compat/abc.py
+++ class_namespaces/compat/abc.py
@@ -6,8 +6,10 @@ from .. import NamespaceableMeta
 
 
 class NamespaceableABCMeta(abc.ABCMeta, NamespaceableMeta):
+
     """Metaclass for Namespaceable classes that are also ABCs."""
 
 
 class NamespaceableABC(metaclass=NamespaceableABCMeta):
+
     """Optional convenience class. Inherit from it to get the metaclass."""
