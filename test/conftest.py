c2de36e6ad7a4450ac28659c37af8fed3dc17df9
Max Chase <max.chase@gmail.com>
2016-12-07 14:55:40 -0500

    Doc conftest.

--- test/conftest.py
+++ test/conftest.py
@@ -1,19 +1,25 @@
+"""Common test fixtures."""
+
+
 import pytest
 
 
 @pytest.fixture
 def namespaces():
+    """Return the top-level class_namespaces module."""
     import class_namespaces
     return class_namespaces
 
 
 @pytest.fixture
 def namespace(namespaces):
+    """Return the Namespace class."""
     return namespaces.Namespace
 
 
 @pytest.fixture(params=['main', 'abc'])
 def namespaceable(request, namespaces, abc):
+    """Return either helper class."""
     if request.param == 'main':
         return namespaces.Namespaceable
     else:
@@ -22,11 +28,13 @@ def namespaceable(request, namespaces, abc):
 
 @pytest.fixture
 def compat():
+    """Return the top-level metaclasses module."""
     import class_namespaces.compat
     return class_namespaces.compat
 
 
 @pytest.fixture
 def abc():
+    """Return the ABC compatibility module."""
     import class_namespaces.compat.abc
     return class_namespaces.compat.abc
